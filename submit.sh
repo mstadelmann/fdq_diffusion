408e31e2e7ec7154f2fb0ba73ace1be594e3ff4e
marc <marc.stadelman@gmail.com>
2025-05-23 01:30:33 +0200

    add submit script

---
+++ submit.sh
@@ -0,0 +1,9 @@
+
+
+python fdq_submit.py /cluster/home/stmd/dev/fdq_diffusion/celeb_gen/generate_faces_UHR_att.json
+34564 -> bad, manually stopped
+
+
+
+python fdq_submit.py /cluster/home/stmd/dev/fdq_diffusion/celeb_gen/generate_faces_HR_long.json
+34471 -> bad, manually stopped
